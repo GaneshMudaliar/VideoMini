e6b67eae71e3250f2e707b174b3f123cd9efdb3d
GaneshMudaliar <mudaliarganesh858@gmail.com>
2025-03-25 12:44:18 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ const express = require("express");
 
 const app = express();
 const cors = require("cors");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
 const PORT = process.env.PORT;
 const dbConnect = require("./config/database");
 const routes = require("./routes/video.route");
 app.use(express.json());
 
+dotenv.config();
 app.use(cors());
 
 // db
